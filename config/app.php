9b9a2c7b3d4cab86644ca16b7ec68714eb518db1
Boey Kok Chin <boeykokchin@live.com.sg>
2020-11-17 12:18:28 +0800

    remove debugbar

--- config/app.php
+++ config/app.php
@@ -176,11 +176,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        /*
-        * Barryvdh Debugbar
-        */
-        Barryvdh\Debugbar\ServiceProvider::class,
-
     ],
 
     /*
